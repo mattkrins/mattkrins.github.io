fa1b05ae3a3b9973f5af85edc8fe0b6fe89d5770
Matt Krins <mattkrins@gmail.com>
2017-03-16 15:06:58 +1100

    Refuse robots + update

--- vacancies/js/index.js
+++ vacancies/js/index.js
@@ -1,41 +1,8 @@
-function getGeoData( bounds, data ) {
-	var geocoder =  new google.maps.Geocoder();
-	geocoder.geocode( { 'address': data.Name}, function(results, status) {
-		if (status == google.maps.GeocoderStatus.OK) {
-			var lat = results[0].geometry.location.lat();
-			var lng = results[0].geometry.location.lng();
-			var latLng = new google.maps.LatLng(lat, lng); 
-			var circle = new google.maps.Circle({
-				strokeColor: '#000000',
-				strokeOpacity: 0.25,
-				strokeWeight: 1.0,
-				fillColor: '#ffffff',
-				fillOpacity: 0.1,
-				clickable: false,
-				map: window.map,
-				center: latLng,
-				radius: 1500
-			});
-			var image = 'school.png';
-			var marker = new google.maps.Marker({
-				position: latLng,
-				map: window.map,
-				icon: image,
-				title: data.content
-			});
-			bounds.extend(marker.position);
-			window.map.fitBounds(bounds);
-			infoBox(marker, data);
-			circle.bindTo('center', marker, 'position');
-		}
-	});	
-}
 function initialize() {
     window.map = new google.maps.Map(document.getElementById('map-canvas'), {
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 	var bounds = new google.maps.LatLngBounds();
-	
 	var myUrl = 'https://employment.bytewize.com.au/recruit/downloadrssfeed?digest=3sTZVsHKxHFiY6oVylxFfhWMAsezGsogx5ji7Hc6ZVY-';
 	$.get('https://cors-anywhere.herokuapp.com/' + myUrl, function( data ) {
 		$(data).find("item").each(function () {
@@ -62,7 +29,7 @@ function initialize() {
 			}
 			for (var i = 0, length = Schools.length; i < length; i++) {
 				var data = Schools[i];
-				getGeoData( bounds, data );
+				makeGeoMarker( bounds, data );
 			}
 		});
 	});
@@ -70,8 +37,26 @@ function initialize() {
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
+function makeGeoMarker( bounds, data ) {
+	var geocoder =  new google.maps.Geocoder();
+	geocoder.geocode( { 'address': data.Name}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			var latLng = new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng()); 
+			var marker = new google.maps.Marker({
+				position: latLng,
+				map: window.map,
+				icon: 'school.png',
+				title: data.Name
+			});
+			bounds.extend(marker.position);
+			window.map.fitBounds(bounds);
+			buildInfoBox(marker, data);
+		}
+	});	
+}
+
 var infowindow
-function infoBox(marker, data) {
+function buildInfoBox(marker, data) {
 	if(infowindow){infowindow.close();}
 	infoWindow = new google.maps.InfoWindow();
 	google.maps.event.addListener(marker, "click", function(e){
