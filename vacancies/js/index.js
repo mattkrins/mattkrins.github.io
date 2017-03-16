48c90d8758959e4bd3db7bdb35b3c5117740c8c2
Matt Krins <mattkrins@gmail.com>
2017-03-16 15:32:51 +1100

    Functional update

--- vacancies/js/index.js
+++ vacancies/js/index.js
@@ -2,71 +2,65 @@ function initialize() {
     window.map = new google.maps.Map(document.getElementById('map-canvas'), {
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-	var bounds = new google.maps.LatLngBounds();
+	window.mapBounds = new google.maps.LatLngBounds();
 	var myUrl = 'https://employment.bytewize.com.au/recruit/downloadrssfeed?digest=3sTZVsHKxHFiY6oVylxFfhWMAsezGsogx5ji7Hc6ZVY-';
 	$.get('https://cors-anywhere.herokuapp.com/' + myUrl, function( data ) {
 		$(data).find("item").each(function () {
 			var str = $(this).find("description").text();
-			var jqueryObject = $.parseHTML( str )
-			var yes = $(jqueryObject).find("span").html();
-			var Schools = [];
-			if (yes){ // Proper XML object
-				var html = $.parseHTML(yes);
+			var htmlObject = $.parseHTML( str )
+			var RSSObject = $(htmlObject).find("span").html();
+			var schoolName = "Error";
+			if (RSSObject){ // Proper RSS object
+				var html = $.parseHTML(RSSObject);
 				var text = $(html).text();
-				var School = [];
-				School["Name"] = text;
-				School["Link"] = $(this).find("link").text();
-				Schools.push(School);
+				schoolName = text;
 			}else{ // Unformatted string
 				var html = $.parseHTML(str.substr(str.indexOf("<br><br><br>") + 12));
 				var text = $(html).text();
 				text = text.replace('Details Apply Now','');
 				text = text.replace(/\-.*/,'');
-				var School = [];
-				School["Name"] = text;
-				School["Link"] = $(this).find("link").text();
-				Schools.push(School);
+				schoolName = text;
 			}
-			for (var i = 0, length = Schools.length; i < length; i++) {
-				var data = Schools[i];
-				makeGeoMarker( bounds, data );
-			}
+			var School = [];
+			School["BytewizeLink"] = $(this).find("link").text();
+			School["Name"] = schoolName;
+			buildGeoMarker( School );
 		});
 	});
 
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
-function makeGeoMarker( bounds, data ) {
+function buildGeoMarker( School ) {
 	var geocoder =  new google.maps.Geocoder();
-	geocoder.geocode( { 'address': data.Name}, function(results, status) {
+	geocoder.geocode( { 'address': School.Name}, function(results, status) {
 		if (status == google.maps.GeocoderStatus.OK) {
 			var latLng = new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng()); 
 			var marker = new google.maps.Marker({
 				position: latLng,
 				map: window.map,
 				icon: 'school.png',
-				title: data.Name
+				title: School.Name
 			});
-			bounds.extend(marker.position);
-			window.map.fitBounds(bounds);
-			buildInfoBox(marker, data);
+			window.mapBounds.extend(marker.position);
+			window.map.fitBounds(window.mapBounds);
+			buildInfoBox(marker, School);
 		}
 	});	
 }
 
 var infowindow
-function buildInfoBox(marker, data) {
+function buildInfoBox(marker, School) {
 	if(infowindow){infowindow.close();}
 	infoWindow = new google.maps.InfoWindow();
 	google.maps.event.addListener(marker, "click", function(e){
-		infoWindow.setContent("<b>"+data.Name+"</b><br/><a target='_newtab' href='"+data.Link+"'>Bytewize Info</a>");
+		infoWindow.setContent("<b>"+School.Name+"</b><br/><a target='_newtab' href='"+School.BytewizeLink+"'>Bytewize Info</a>");
 		infoWindow.open(window.map, marker);
 	});
-	(function(marker, data) {
+	(function(marker, School) {
 		google.maps.event.addListener(marker, "click", function(e){
-			infoWindow.setContent("<b>"+data.Name+"</b><br/><a target='_newtab' href='"+data.Link+"'>Bytewize Info</a>");
+			infoWindow.setContent("<b>"+School.Name+"</b><br/><a target='_newtab' href='"+School.BytewizeLink+"'>Bytewize Info</a>");
 			infoWindow.open(window.map, marker);
 	  });
-	})(marker, data);
+	})(marker, School);
 }
