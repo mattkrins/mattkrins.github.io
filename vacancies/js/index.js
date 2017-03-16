e2c30eafa2c1400544ed0973291b285f8c8ece1d
Matt Krins <mattkrins@gmail.com>
2017-03-16 17:36:53 +1100

    Added info, image icons & traffic button

--- vacancies/js/index.js
+++ vacancies/js/index.js
@@ -1,6 +1,21 @@
+var showingTraffic = false;
+$( ".trafficButton" ).click(function() {
+	if (!window.trafficLayer){return;}
+	if (showingTraffic){
+		window.trafficLayer.setMap(null);
+		$(this).css("font-weight","normal");
+	}else{
+		window.trafficLayer.setMap(window.map);
+		$(this).css("font-weight","bold");
+	}
+	showingTraffic = !showingTraffic;
+});
+
 function initializeMap() {
     window.map = new google.maps.Map(document.getElementById('map-canvas'), { mapTypeId: google.maps.MapTypeId.ROADMAP });
 	window.mapBounds = new google.maps.LatLngBounds();
+	window.trafficLayer = new google.maps.TrafficLayer();
+	
 	var bytewizeUrl = 'https://employment.bytewize.com.au/recruit/downloadrssfeed?digest=3sTZVsHKxHFiY6oVylxFfhWMAsezGsogx5ji7Hc6ZVY-';
 	var corsUrl = 'https://cors-anywhere.herokuapp.com/'; // Use a proxy because nobody likes to share data.
 	$.get(corsUrl + bytewizeUrl, function( data ) {
@@ -47,18 +62,54 @@ function buildGeoMarker( School ) {
 	});	
 }
 
+
+function getPhoto(place, w, h) {
+	var photo = false;
+	if ( (place) && (place.photos) ) { photo = place.photos[0].getUrl({'maxWidth': w, 'maxHeight': h}); }
+	return photo;
+}
+
+function populateSchoolInfo(marker, School) {
+	var request = {location: marker.position,radius: '500',query: School.Name};
+	var service = new google.maps.places.PlacesService(window.map);
+	service.textSearch(request, function(results, status){
+		if (status != google.maps.places.PlacesServiceStatus.OK) {return;}
+		var place = results[0];
+		var photo = getPhoto(place, 32, 32);
+		marker.setMap(null);
+		if (place){ marker.place = place; }
+		if (photo){ marker.icon = photo; }
+		marker.setMap(window.map);
+	});
+}
+
 var infowindow
-function buildInfoBox(marker, School) {
+function showInfoBox(marker, School) {
 	if(infowindow){infowindow.close();}
+	var textBox = "";
+	if (marker.place){
+		var photo = getPhoto(marker.place, 200, 100);
+		if (photo){ textBox = textBox+"<img src='"+photo+"' /><br/>"; }
+		if (marker.place.icon){textBox = textBox+"<img with='10' height='10' src='"+marker.place.icon+"' /> ";}
+	}
+	textBox = textBox+"<b>"+School.Name+"</b><br/>";
+	if ((marker.place) && (marker.place.formatted_address)){
+		textBox = textBox+"<a target='_newtab' href='https://maps.google.com?daddr="+marker.place.formatted_address+"&layer=t'>Google Navigate</a><br/>";
+	}
+	textBox = textBox+"<a target='_newtab' href='"+School.BytewizeLink+"'>Bytewize Info</a>";
+	infoWindow.setContent(textBox);
+	infoWindow.open(window.map, marker);
+}
+function buildInfoBox(marker, School) {
+	populateSchoolInfo(marker, School);
 	infoWindow = new google.maps.InfoWindow();
 	google.maps.event.addListener(marker, "click", function(e){
-		infoWindow.setContent("<b>"+School.Name+"</b><br/><a target='_newtab' href='"+School.BytewizeLink+"'>Bytewize Info</a>");
-		infoWindow.open(window.map, marker);
+		showInfoBox(marker, School);
 	});
 	(function(marker, School) {
 		google.maps.event.addListener(marker, "click", function(e){
-			infoWindow.setContent("<b>"+School.Name+"</b><br/><a target='_newtab' href='"+School.BytewizeLink+"'>Bytewize Info</a>");
-			infoWindow.open(window.map, marker);
+			showInfoBox(marker, School);
 	  });
 	})(marker, School);
 }
+
